6de395ff63cc459fad31b016525af323dfaf0a86
S1riusB <stassokolov05@gmail.com>
2023-04-23 12:53:12 +0300

    add task

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    double start_time = clock();
+    for (int i = 0; i <= 100; i++)
+    {
+        cout << i << " ";
+    }
+    double end_time = clock();
+    double search_time = end_time - start_time;
+    cout << "\nTime is: " << search_time << " sec";
+}
